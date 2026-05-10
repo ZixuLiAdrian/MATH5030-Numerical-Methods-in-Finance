1538206abc63a513795f35f0c1f5ac3bb0a23df1
chanpengchen <chanpeng.chen1@gmail.com>
2026-05-10 12:58:22 -0400

    utils.py

--- src/asian_option_pricer/utils.py
+++ src/asian_option_pricer/utils.py
@@ -1,9 +1,20 @@
 import numpy as np
+
 from .models import AsianOptionParams
 
 
 def monitoring_times(params: AsianOptionParams) -> np.ndarray:
-    return np.arange(1, params.N + 1) * (params.T / params.N)
+    """
+    Return equally spaced monitoring dates between T1 and T2.
+    """
+
+    params.validate()
+
+    T2 = params.averaging_end
+
+    dt = (T2 - params.T1) / params.N
+
+    return params.T1 + np.arange(1, params.N + 1) * dt
 
 
 def discount_factor(params: AsianOptionParams) -> float:
