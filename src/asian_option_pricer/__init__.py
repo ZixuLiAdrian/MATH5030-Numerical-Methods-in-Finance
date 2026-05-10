65608c223833be8a09adfd680a3a7c9579e4e163
chanpengchen <chanpeng.chen1@gmail.com>
2026-05-10 13:24:28 -0400

    __init__.py

--- src/asian_option_pricer/__init__.py
+++ src/asian_option_pricer/__init__.py
@@ -1,28 +1,61 @@
 """Efficient numerical pricing methods for arithmetic Asian options."""
-from .analytic import geometric_asian_call_price, levy_approx_call_price
-from .control_variate import antithetic_cv_price, control_variate_price
-from .models import AsianOptionParams
-from .monte_carlo import antithetic_mc_price, standard_mc_price
+
+from .analytic import (
+    geometric_asian_call_price,
+    levy_approx_call_price,
+)
+
+from .control_variate import (
+    antithetic_cv_price,
+    control_variate_price,
+)
+
+from .models import (
+    AsianOptionParams,
+)
+
+from .monte_carlo import (
+    antithetic_mc_price,
+    standard_mc_price,
+)
+
 from .paths import (
     brownian_bridge_matrix,
+    brownian_bridge_matrix_from_times,
     build_paths,
     geometric_payoff_from_paths,
     payoff_from_paths,
 )
-from .qmc import rqmc_sobol_price, sobol_qmc_price
+
+from .qmc import (
+    rqmc_sobol_price,
+    sobol_qmc_price,
+)
+
 
 __all__ = [
     "AsianOptionParams",
-    "antithetic_cv_price",
-    "antithetic_mc_price",
-    "brownian_bridge_matrix",
-    "build_paths",
-    "control_variate_price",
+
+    # analytic
     "geometric_asian_call_price",
-    "geometric_payoff_from_paths",
     "levy_approx_call_price",
-    "payoff_from_paths",
-    "rqmc_sobol_price",
-    "sobol_qmc_price",
+
+    # monte carlo
     "standard_mc_price",
+    "antithetic_mc_price",
+
+    # control variates
+    "control_variate_price",
+    "antithetic_cv_price",
+
+    # qmc
+    "sobol_qmc_price",
+    "rqmc_sobol_price",
+
+    # paths
+    "brownian_bridge_matrix",
+    "brownian_bridge_matrix_from_times",
+    "build_paths",
+    "payoff_from_paths",
+    "geometric_payoff_from_paths",
 ]
